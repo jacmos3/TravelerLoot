20dffcb0494ba97986dd43e02a261196998ed24b
Jacopo <jacopogithub.pbcc5@aleeas.com>
2022-02-04 19:17:59 +0100

    livestream moved to under the claim button

--- components/IndexSections/Presentation.js
+++ components/IndexSections/Presentation.js
@@ -34,20 +34,11 @@ render(){
   return (
     <div className="mx-auto px-2 sm:px-4 text-center py-10 sm:py-20 justify-around flex flex-wrap hero-img bg-opacity-10">
       <div className="w-full sm:w-1/2">
-        <Message info>
-          <h2>Watch the Live Streaming</h2>
-          <Embed
-             id='mMzH-H-Jg2M'
-             source='youtube'
-             placeholder='../../img/preview.png'
-           />
-        </Message>
         <div className="sm:px-4">
           <div className={styles.home__cta}>
             <h1 className="interline">Traveler Loot</h1>
             <p className="text-xl sm:text-2xl text-white">
               <br />
-
               Traveler Loot is a <a target= "_blank" href="https://www.lootproject.com/">Loot</a> derivative for the travel industry, generated and stored on chain.
               Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Traveler Loot in any way you want.
             </p>
@@ -83,7 +74,14 @@ render(){
               <Button className=" hover:text-white  mx-2" secondary >Claim a Traveler Loot</Button>
             </a>
           </div>
-
+          <Message info>
+            <h2>Watch the Live Streaming</h2>
+            <Embed
+               id='mMzH-H-Jg2M'
+               source='youtube'
+               placeholder='../../img/preview.png'
+             />
+          </Message>
         </div>
       </div>
     </div>
